d2325273d2a6b45142f6c5cdb4c3e0af1c08a887
DIEGO FERNANDES <diegocruzfernandes@hotmail.com>
2018-01-07 23:06:01 -0200

    Add Repository

---
+++ src/StoreOfBuild.DI/Bootstrap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StoreOfBuild.Data;
+using StoreOfBuild.Domain;
+using StoreOfBuild.Domain.Products;
+
+namespace StoreOfBuild.DI
+{
+    public class Bootstrap
+    {
+        public static void Configure(IServiceCollection services, string connection)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(connection));
+
+            services.AddSingleton(typeof(IRepository<>), typeof(Repository<>));
+            services.AddSingleton(typeof(CategoryStorer));
+            services.AddSingleton(typeof(IUnitOfWork), typeof(UnitOfWork));
+        }
+    }
+}
